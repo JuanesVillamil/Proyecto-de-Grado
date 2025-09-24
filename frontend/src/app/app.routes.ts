89a44c7d56cc332a157a279bb9598df2566d0d6a
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-09-23 21:24:48 -0500

    v1.1.2
    Componente Results eliminado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UploadComponent } from './components/upload/upload.component';
-import { ResultsComponent } from './components/results/results.component';
 import { ReportComponent } from './components/report/report.component';
 import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'upload', component: UploadComponent },
-  { path: 'results', component: ResultsComponent },
   { path: 'report', component: ReportComponent },
   { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '' }
