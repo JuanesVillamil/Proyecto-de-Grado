77db1df3f5c71a5c83ece2f3151ee5955b898af0
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-10-08 23:59:24 -0500

    v1.3.2
    Instrucciones para entorno virtual

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ cd ..
 
 2. Iniciar Uvicorn
 cd backend
+Conexion a entorno virtual source venv/bin/activate (MacOs) o venv\Scripts\activate (Windows)
 uvicorn app:app --host 127.0.0.1 --port 8000
 
 3. Iniciar proyecto
--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://juanvillamil@localhost:5432/birads_db"
+DATABASE_URL = "postgresql+psycopg2://postgres@localhost:5432/birads_db"
 
 engine = create_engine(DATABASE_URL)
 
